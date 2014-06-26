40ecba9b47822bb0dab537e952bbd7dce21c02ad
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-06-26 16:21:43 +0200

    extremly ugly codewise (but working) carousel

--- 25_carousel.js
+++ 25_carousel.js
@@ -1,7 +1,85 @@
 /*global $ */
 
+var DOM = {
+	"carouselWrapper": $("#carousel >.carousel-wrapper"),
+	"buttonPrev": $("#carousel > .button-prev"),
+	"buttonNext": $("#carousel > .button-next")
+};
+
+var CAROUSEL_DATA = [];
+var CAROUSEL_INDEX = 0;
+
+var buildItem = function(data) {
+	var carouselItem = "<div class=\"carousel-item\">";
+	// carouselItem += "<img src=\"" +
+	carouselItem += "<span>" + data.name + "</span>";
+	carouselItem += "</div>";
+
+	return carouselItem;
+};
+
+var buildCarousel = function() {
+	// add first three items to DOM
+	CAROUSEL_DATA.slice(0, 3).forEach(function(object, index) {
+		DOM.carouselWrapper.append(
+			$(buildItem(object)).css({
+				"position": "absolute",
+				"left": index * 322 + (index > 0 ? index * 14 : 0) + "px"
+			})
+		);
+	});
+};
+
+DOM.buttonPrev.on("click", function() {
+	var prependIndex = CAROUSEL_INDEX + 4;
+	if (prependIndex > CAROUSEL_DATA.length - 1) { prependIndex = 0; }
+	CAROUSEL_INDEX = prependIndex;
+
+	DOM.carouselWrapper
+		.append(buildItem(CAROUSEL_DATA[prependIndex]))
+		.children().each(function(index, child) {
+			$(child)
+				.css({
+					"position": "absolute",
+					"left": index * 322 + (index > 0 ? index * 14 : 0) + "px"
+				})
+				.animate({
+					"left": (index - 1) * 322 + (index >= 0 ? index * 14 : 0) + "px"
+				},
+				{
+					"complete": function() {
+						if (index === 0) { $(child).remove(); }
+					}
+				});
+		});
+});
+
+DOM.buttonNext.on("click", function() {
+	var prependIndex = CAROUSEL_INDEX - 1;
+	if (prependIndex < 0) { prependIndex = CAROUSEL_DATA.length - 1; }
+	CAROUSEL_INDEX = prependIndex;
+
+	DOM.carouselWrapper
+		.prepend(buildItem(CAROUSEL_DATA[prependIndex]))
+		.children().each(function(index, child) {
+			$(child)
+				.css({
+					"position": "absolute",
+					"left": (index - 1) * 322 + (index >= 0 ? index * 14 : 0) + "px"
+				})
+				.animate({
+					"left": index * 322 + (index > 0 ? index * 14 : 0) + "px"
+				},
+				{
+					"complete": function() {
+						if (index === 3) { $(child).remove(); }
+					}
+				});
+		});
+});
+
 var parseData = function(data) {
-	data = data.page.children.map(function(data) {
+	return data.page.children.map(function(data) {
 		var techFocus = data.custom_fields["tech-focus"];
 
 		if (techFocus) {
@@ -28,10 +106,11 @@ var parseData = function(data) {
 			"attachments": data.attachments
 		};
 	});
-
-	console.log(data);
 };
 
 // fetch data
 var API_URL = "http://content.digitalsocial.eu/api/get_page/?slug=case-studies&children=true";
-$.getJSON(API_URL, parseData);
+$.getJSON(API_URL, function(data) {
+	CAROUSEL_DATA = parseData(data);
+	buildCarousel();
+});
