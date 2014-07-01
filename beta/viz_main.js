91e8180717c8d96d3906bdef6d9edaa34a8115e5
Marcin Ignac <marcin.ignac@gmail.com>
2014-07-01 13:54:21 +0100

    Playing with iso lines

--- beta/viz_main.js
+++ beta/viz_main.js
@@ -6,6 +6,10 @@ function init() {
   getOrganisations().then(handleResults);
 }
 
+function hsla(h, s, l, a) {
+  return 'hsla(' + h + ',' + 100 * s + '%,' + 100 * l + '%,' + a + ')';
+}
+
 function resultValuesToObj(result) {
   var o = {};
   for (var prop in result) {
@@ -88,8 +92,8 @@ function buildViz(organisations) {
   var g = svg.append('g');
 
   var zoom = addZoom(svg, g, w, h);
-
-  showOrganisations(g, projection, center, organisations, zoom);
+  showOrganisations(svg, g, projection, center, organisations, zoom);
+  showIsoLines(svg, g, organisations, w, h);
 }
 
 function addZoom(svg, g, w, h) {
@@ -138,6 +142,7 @@ function addZoom(svg, g, w, h) {
 
     if (scale < 0.5) {
       zoom.scale(0.5);
+      zoom.event(svg);
     }
   }
 
@@ -150,6 +155,7 @@ function addZoom(svg, g, w, h) {
     d3.event.preventDefault();
     var scale = 2;
     var newZoom = zoom.scale() * scale;
+    if (newZoom > 2048) return;
     var newX = ((zoom.translate()[0] - (w / 2)) * scale) + w / 2;
     var newY = ((zoom.translate()[1] - (h / 2)) * scale) + h / 2;
     zoom.scale(newZoom);
@@ -163,11 +169,12 @@ function addZoom(svg, g, w, h) {
     d3.event.preventDefault();
     var scale = 1/2;
     var newZoom = zoom.scale() * scale;
+    if (newZoom < 0.3) return;
     var newX = ((zoom.translate()[0] - (w / 2)) * scale) + w / 2;
     var newY = ((zoom.translate()[1] - (h / 2)) * scale) + h / 2;
     zoom.scale(newZoom);
     zoom.translate([newX, newY])
-    zoom.event(svg)
+    zoom.event(svg);
     return false;
   })
 
@@ -177,13 +184,13 @@ function addZoom(svg, g, w, h) {
   return zoom;
 }
 
-function showOrganisations(g, projection, center, organisations, zoom) {
+function showOrganisations(svg, g, projection, center, organisations, zoom) {
   var circles = g.selectAll('circle.org').data(organisations)
 
   circles.enter()
     .append('circle')
     .attr('class', 'org')
-    .attr('r', 5)
+    .attr('r', 2)
     .attr('cx', 0)
     .attr('cy', 0)
     .attr('transform', function(d) {
@@ -208,277 +215,139 @@ function showOrganisations(g, projection, center, organisations, zoom) {
 
   zoom.on('zoom.circles', function() {
     circles.attr('r', function() {
-      return 5 /  d3.event.scale;
+      return 2 /  d3.event.scale;
     })
   });
 }
 
-function initOld() {
-  if (useCache && localStorage['data_19']) {
-    allResults = JSON.parse(localStorage['data_19']);
-    buildViz(allResults);
+function showIsoLines(svg, g, organisations, w, h) {
+  var randomPoints;
+  var numPoints = 4002;
+
+  var color = hsla(0, 0, 0, 0.2);
+
+  if (localStorage['randomPoints'] && localStorage['randomPoints'] != 'null') {
+    randomPoints = JSON.parse(localStorage['randomPoints']);
+    if (randomPoints.length != numPoints) {
+      randomPoints = null;
+    }
   }
-  else {
-    d3.json('data/org_query.json', function(data) {
-      allResults = data.results.bindings;
-      var orgHashMap = {};
-      var activityHashMap = {};
-      allResults.forEach(function(org) {
-        if (!orgHashMap[org.org.value]) {
-          orgHashMap[org.org.value] = org;
-          orgHashMap[org.org.value].activities = [];
-          orgHashMap[org.org.value].dsiAreas = [];
-        }
-        var orgData = orgHashMap[org.org.value];
-        var activityData = activityHashMap[org.activity.value];
-        if (!activityData) {
-          activityData = {
-            label: org.activity.value,
-            technologyFocus: [],
-            dsiAreas: []
-          }
-          activityHashMap[org.activity.value] = activityData;
-        }
-        activityData.technologyFocus.push(org.tf.value);
-        orgData.activities.push(activityData);
-      })
-      var groupedResults = [];
-      for(var orgId in orgHashMap) {
-        groupedResults.push(orgHashMap[orgId]);
+  if (!randomPoints) {
+    var d = 30;
+    randomPoints = d3.range(0, numPoints).map(function(i) {
+      return [ Math.random() * w, Math.random() * h, 1];
+      //return [ 200 + (i % d)/d * (w - 200), Math.floor(i / d)/d * h, 1];
+    });
+    localStorage['randomPoints'] = JSON.stringify(randomPoints);
+  }
+
+  var minR = 100;
+  var minR2 = minR * minR;
+
+  randomPoints.forEach(function(p) {
+    organisations.forEach(function(org) {
+      var dx = p[0] - org.x;
+      var dy = p[1] - org.y;
+      if (dx*dx + dy*dy < minR2) {
+        p[2]++;
       }
-      //localStorage['data_18'] = JSON.stringify(groupedResults);
-      allResults = groupedResults;
-
-      console.log(activityHashMap)
-
-      DSIAreasData.forEach(function(row) {
-        var activity = 'http://data.digitalsocial.eu/id/activity/' + row[0];
-        var dsiArea = row[1];
-        var activityData = activityHashMap[activity];
-        if (activityData) {
-          activityData.dsiAreas.push(dsiArea);
-        }
-      })
-
-      for(var orgId in orgHashMap) {
-        var org = orgHashMap[orgId];
-        org.activities.forEach(function(a){
-          a.dsiAreas.forEach(function(area) {
-            org.dsiAreas.push(area);
-          })
-        })
-      }
-
-      buildViz(allResults);
     })
-  }
-}
-
-function hsla(h, s, l, a) {
-  return 'hsla(' + h + ',' + 100 * s + '%,' + 100 * l + '%,' + a + ')';
-}
-
-var svg;
-
-function buildVizOld(results, color) {
-  var hue = 229;
-  color = color || '#DDDDDD';
-  console.log('buildViz', results.length);
-  
-
-  d3.select('#sidebar').style('height', h + 'px');
-
-  var longitude = d3.scale.linear().domain([-20, 30]).range([0, w]);
-  var latitude = d3.scale.linear().domain([65, 30]).range([0, h]);
-
-  results.forEach(function(org) {
-    org.x = longitude(org.long.value);
-    org.y = latitude(org.lat.value);
   })
 
-  var cities = [];
-  var cityInfo = {};
-  var orgTypes = [];
+  var points = g.selectAll('circle.triPoint').data(randomPoints);
+  points.enter()
+    .append('circle')
+    .attr('class', 'triPoint')
+    //.style('fill', 'rgba(255,0,0,0.2)');
+    .style('fill', 'none')
+    .style('stroke', 'rgba(255,0,0,0.2)');
 
-  results.forEach(function(org) {
-    var type = org.org_type.value;
-    var city = org.city.value;
-    if (orgTypes.indexOf(type) == -1) orgTypes.push(type);
-    if (!cityInfo[city]) {
-      cityInfo[city] = { name: city, lat: 0, long:0, organisations: [] };
-      cities.push(cityInfo[city]);
-    }
-    cityInfo[city].organisations.push(org);
-    cityInfo[city].lat += Number(org.lat.value);
-    cityInfo[city].long += Number(org.long.value);
-  });
+  points.exit().remove()
 
-  cities.forEach(function(city) {
-    city.lat /= city.organisations.length;
-    city.long /= city.organisations.length;
-    if (city.name == 'London') {
+  points
+    .attr('cx', function(d) { return d[0]; })
+    .attr('cy', function(d) { return d[1]; })
+    //.attr('r', function(d) { return Math.pow(d[2], 0.47); })
+    .attr('r', minR)
+    //.style('display', 'none');
 
+  var triangles = d3.geom.delaunay(randomPoints);
+  var toArrayOfPoints = function(p) { return [p[0], p[1]]; };
+
+  var linePoints = [];
+
+  function sections(p1, p2, mod, p3) {
+    var val1 = p1[2];
+    var val2 = p2[2];
+    var val3 = p3[2];
+    if (val1 < val2) {
+      var start = Math.ceil(val1 / mod) * mod;
+      var dist = val2 - val1;
+      var results = [];
+      for(var v = start; v <= val2; v += mod) {
+        var ratio = (val1 == val2) ? 0 : (v-val1)/(val2-val1);
+        results.push({ value: v, ratio: ratio, point: [p1[0]+(p2[0]-p1[0])*ratio, p1[1]+(p2[1]-p1[1])*ratio] });
+      }
+      return results;
     }
     else {
-      //city.lat = 9999;
-    }
-  });
-
-  buildIsoLines(results, svg, longitude, latitude, color);
-}
-
-  //gen random points
-  //triangulate
-  //count number of nearby projects
-  //grow circle accordingly
-
-  //? uniform triangulation
-
-  function buildIsoLines(results, svg, longitude, latitude, color) {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var randomPoints;
-    var numPoints = 4002;
-
-    if (localStorage['randomPoints'] && localStorage['randomPoints'] != 'null') {
-      randomPoints = JSON.parse(localStorage['randomPoints']);
-      if (randomPoints.length != numPoints) {
-        randomPoints = null;
+      var start = Math.ceil(val2 / mod) * mod;
+      var dist = val1 - val2;
+      var results = [];
+      for(var v = start; v <= val1; v += mod) {
+        var ratio = (val1 == val2) ? 0 : 1.0-(v-val2)/(val1-val2);
+        if (ratio == 0 && (val1 == val2 && val2 == val3)) continue;
+        results.push({ value: v, ratio: ratio, point: [p1[0]+(p2[0]-p1[0])*ratio, p1[1]+(p2[1]-p1[1])*ratio] });
       }
+      return results;
     }
-    if (!randomPoints) {
-      var d = 30;
-      randomPoints = d3.range(0, numPoints).map(function(i) {
-        return [ 200 + Math.random() * (w - 200), Math.random() * h, 1];
-        //return [ 200 + (i % d)/d * (w - 200), Math.floor(i / d)/d * h, 1];
-      });
-      localStorage['randomPoints'] = JSON.stringify(randomPoints);
-    }
+  }
 
-    var minR = 100;
-    var minR2 = minR * minR;
-
-    randomPoints.forEach(function(p) {
-      results.forEach(function(org) {
-        var dx = p[0] - org.x;
-        var dy = p[1] - org.y;
-        if (dx*dx + dy*dy < minR2) {
-          p[2]++;
-        }
-      })
-    })
-
-    var points = svg.selectAll('circle.triPoint').data(randomPoints);
-    points.enter()
-      .append('circle')
-      .attr('class', 'triPoint')
-      .style('fill', 'rgba(255,0,0,0.2)');
-
-    points.exit().remove()
-
-    points
-      .attr('cx', function(d) { return d[0]; })
-      .attr('cy', function(d) { return d[1]; })
-      .attr('r', function(d) { return Math.pow(d[2], 0.7); })
-      .style('display', 'none');
-
-    var triangles = d3.geom.delaunay(randomPoints);
-    var toArrayOfPoints = function(p) { return [p[0], p[1]]; };
-
-    var linePoints = [];
-
-    function sections(p1, p2, mod, p3) {
-      var val1 = p1[2];
-      var val2 = p2[2];
-      var val3 = p3[2];
-      if (val1 < val2) {
-        var start = Math.ceil(val1 / mod) * mod;
-        var dist = val2 - val1;
-        var results = [];
-        for(var v = start; v <= val2; v += mod) {
-          var ratio = (val1 == val2) ? 0 : (v-val1)/(val2-val1);
-          results.push({ value: v, ratio: ratio, point: [p1[0]+(p2[0]-p1[0])*ratio, p1[1]+(p2[1]-p1[1])*ratio] });
-        }
-        return results;
-      }
-      else {
-        var start = Math.ceil(val2 / mod) * mod;
-        var dist = val1 - val2;
-        var results = [];
-        for(var v = start; v <= val1; v += mod) {
-          var ratio = (val1 == val2) ? 0 : 1.0-(v-val2)/(val1-val2);
-          if (ratio == 0 && (val1 == val2 && val2 == val3)) continue;
-          results.push({ value: v, ratio: ratio, point: [p1[0]+(p2[0]-p1[0])*ratio, p1[1]+(p2[1]-p1[1])*ratio] });
-        }
-        return results;
-      }
-    }
-
-    function addLines(sections1, sections2) {
-      for(var i=0; i<sections1.length; i++) {
-        for(var j=0; j<sections2.length; j++) {
-          if (sections1[i].value == sections2[j].value) {
-            linePoints.push([sections1[i].point, sections2[j].point, sections2[j].value])
-          }
+  function addLines(sections1, sections2) {
+    for(var i=0; i<sections1.length; i++) {
+      for(var j=0; j<sections2.length; j++) {
+        if (sections1[i].value == sections2[j].value) {
+          linePoints.push([sections1[i].point, sections2[j].point, sections2[j].value])
         }
       }
     }
+  }
 
-    triangles.forEach(function(triangle, triangeIndex) {
-      var p0 = triangle[0];
-      var p1 = triangle[1];
-      var p2 = triangle[2];
-      //if (triangeIndex % 21 == 0) {
-        //linePoints.push([p0, p1]);
-        //console.log('triangeIndex', triangeIndex);
-        //if (triangeIndex != 21 ) return;
-        //if (triangeIndex != 168 ) return;
-        var sections0 = sections(p0, p1, 3, p2);
-        var sections1 = sections(p0, p2, 3, p1);
-        var sections2 = sections(p1, p2, 3, p0);
-        addLines(sections0, sections1);
-        addLines(sections0, sections2);
-        addLines(sections1, sections2);
-      //}
-      //var sections = 
-    })
+  triangles.forEach(function(triangle, triangeIndex) {
+    var p0 = triangle[0];
+    var p1 = triangle[1];
+    var p2 = triangle[2];
+    var sections0 = sections(p0, p1, 6, p2);
+    var sections1 = sections(p0, p2, 6, p1);
+    var sections2 = sections(p1, p2, 6, p0);
+    addLines(sections0, sections1);
+    addLines(sections0, sections2);
+    addLines(sections1, sections2);
+  })
 
-    svg.selectAll("g.edgeLine").remove();
-    var path = svg.append("g").attr("class", 'edgeLine').selectAll("path.edgeLine");
-    path = path.data(triangles.map(function(d) { 
-      return "M" + d.map(toArrayOfPoints).join("L") + "Z"; 
-    }), String);
-    path.exit().remove();
-    path.enter().append("path").attr("d", String);
-    path.style('stroke', 'rgba(255,0,0,0.1)')
-    path.style('fill', 'none');
-    path.style('display', 'none');
-
-    svg.selectAll("g.isoLine").remove();
-    var path = svg.append("g").attr("class", 'isoLine').selectAll("path.isoLine");
-    path = path.data(linePoints.map(function(d) { 
-      return {
+  g.selectAll("g.isoLine").remove();
+  var path = g.append("g").attr("class", 'isoLine').selectAll("path.isoLine");
+  path = path.data(linePoints.map(function(d) {
+    return {
       data: "M" + toArrayOfPoints(d).join("L") + "",
       value: d[2]
-    }; }));
-    path.exit().remove();
-    path.enter()
-      .append("path")
-      .attr('stroke', function(d) { return hsla(1, 0.52, 0.6, 0.029 + d.value/100) })
-      //.attr('stroke', function(d) { return hsla(hue, 0.52, 0.6, 0.29) })
-      .attr('stroke', color)
-      //.attr('stroke-width', function(d) { return 10.0-Math.sqrt(d.value)})
-      //.attr('stroke-width', 1)
-      .attr("d", function(d) { return d.data });
-    path.style('fill', 'none');
-  }
+    };
+  }));
+  path.exit().remove();
+  path.enter()
+    .append("path")
+    .attr('stroke', color)
+    .attr("d", function(d) { return d.data });
+  path.style('fill', 'none');
+}
 
-  function filter(dsiArea, color) {
-    console.log('filter', dsiArea, color)
-    buildViz(allResults.filter(function(org) {
-      return org.dsiAreas.indexOf(dsiArea) !== -1;
-    }), color);
-  }
 
-  return init;
+function filter(dsiArea, color) {
+  console.log('filter', dsiArea, color)
+  buildViz(allResults.filter(function(org) {
+    return org.dsiAreas.indexOf(dsiArea) !== -1;
+  }), color);
+}
+
+return init;
 })();
