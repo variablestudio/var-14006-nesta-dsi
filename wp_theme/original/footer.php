541934cbcc9e8a7b6f1e1ae7c39e687a9ce058ed
Marcin Ignac <marcin.ignac@gmail.com>
2014-07-01 10:48:56 +0100

    Footer backup

---
+++ wp_theme/original/footer.php
@@ -0,0 +1,58 @@
+</div>
+<div class='hr hr1 hr-full'></div>
+<div id='partners'>
+<a class='nesta' href='http://www.nesta.org.uk/' title='Nesta'></a>
+<a class='esade' href='http://www.esade.edu/' title='Esade Business School'></a>
+<a class='future-everything' href='http://futureeverything.org/' title='FutureEverything'></a>
+<a class='iri' href='http://www.iri.centrepompidou.fr/en/' title="Institut de Recherche et d'Innovation"></a>
+<a class='waag' href='http://waag.org/en' title='Waag Society'></a>
+<a class='swirrl' href='http://www.swirrl.com/' title='Swirrl'></a>
+</div>
+
+<div class='hr hr3 hr-full'></div>
+<footer class='clearfix'>
+<div class='credits'>
+<div class='copyright'>
+<span>
+&copy;
+2014
+<a href="/about">Digital Social Innovation</a>.
+</span>
+</div>
+<div class='contact'>
+<span class='twitter'>
+<i class='icon-social-twitter'></i>
+<a href='https://twitter.com/digi_si'>
+@digi_si
+</a>
+</span>
+<span class='email'>
+<a href='mailto:contact@digitalsocial.eu'>
+<i class='icon-paper-plane'></i>
+contact@digitalsocial.eu
+</a>
+</span>
+</div>
+</div>
+<nav>
+<div style='font-size:14px; float:right;'>
+<a href='http://content.digitalsocial.eu/terms-of-use/'>
+Terms of Use
+</a>
+<a href='http://content.digitalsocial.eu/privacy-cookies/'>
+Privacy &amp; Cookies
+</a>
+</div>
+</nav>
+</footer>
+
+
+</div>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/scripts.js"></script>
+<?php wp_footer();?>
+</body>
+
+</html>
