75202fb965c736bdf4b54f1d501f40d48bbdfb89
Marcin Ignac <marcin.ignac@gmail.com>
2014-07-02 07:58:37 +0100

    Key tooltips

--- beta/viz_config.js
+++ beta/viz_config.js
@@ -20,6 +20,13 @@ VizConfig.dsiAreasById = {
   'open-access': VizConfig.dsiAreas[5]
 };
 
+VizConfig.dsiAreasById['funding-acceleration-and-incubation'].info = '';
+VizConfig.dsiAreasById['collaborative-economy'].info = 'Collaborative economy: New collaborative socio-economic models that present novel characteristics, and enable people to share skills, knowledge, food, clothes, housing and so on. It includes crypto digital currencies, new forms of crowdfunding and financing, new platforms for exchanges and sharing resources based on reputation and trust.';
+VizConfig.dsiAreasById['open-democracy'].info = 'Open democracy is transforming the traditional models of representative democracy. Digital technology can enable collective participation at a scale that was impossible before enabling citizens to be engaged in decision-making processes, collective deliberation, and mass mobilisation. ';
+VizConfig.dsiAreasById['awarness-networks'].info = 'Platforms for collaboration are able to aggregate data coming from people and the environment and are used to solve environmental issues and promote sustainable behavioral changes, or to mobilize collective action and respond to community emergencies. ';
+VizConfig.dsiAreasById['new-ways-of-making'].info = 'An ecosystem of makers is revolutionising open design and manufacturing. 3D manufactur­ing tools, free CAD/CAM software and open source designs are now giving innovators better access to tools, products, skills and capabilities they need to enhance collaborative making.';
+VizConfig.dsiAreasById['open-access'].info = 'The Open Access Ecosystem approach has the potential to empower citizens and increase participation, while preserving privacy-aware and decentralised infrastructures. It includes projects that facilitate the diffusion of knowledge systems in the Public Domain, open standards, open licensing, knowledge commons and digital rights.';
+
 VizConfig.dsiAreasByLabel = {
   'Funding Acceleration and Incubation': VizConfig.dsiAreas[0],
   'Collaborative Economy': VizConfig.dsiAreas[1],
@@ -42,3 +49,8 @@ VizConfig.technologyFocusesById = {
   'open-knowledge': VizConfig.technologyFocuses[2],
   'open-data': VizConfig.technologyFocuses[3]
 };
+
+VizConfig.technologyFocusesById['open-hardware'].info = 'New ways of making and using open hard­ware solutions and moving towards and Open Source Internet of Things';
+VizConfig.technologyFocusesById['open-networks'].info = 'Innovative combinations of network solutions and infrastructures, e.g. sensor net­works, free interoperable network services, open Wifi, bottom-up-broadband, distribut­ed social networks, p2p infrastructures';
+VizConfig.technologyFocusesById['open-knowledge'].info = 'Co-production of new knowledge and crowd mobilisation based on open content, open source and open access';
+VizConfig.technologyFocusesById['open-data'].info = 'Innovative ways to capture, use, analyse, and interpret open data coming from people and from the environment';
--- beta/viz_key.js
+++ beta/viz_key.js
@@ -15,14 +15,32 @@ function VizKey() {
   sideBar.append($('<h3><img src="' + VizConfig.assetsPath + '/key-project-solid.png' + '" height="40"/>' + projectsTitle + '</h3>'));
   sideBar.append($('<h3>' + dsiTitle + '</h3>'));
 
+  var tooltip = this.makeTooltip();
+
   VizConfig.dsiAreas.map(function(dsiArea, dsiAreaIndex) {
-    sideBar.append($('<a style="color:' + dsiArea.color + '"><img src="' + dsiArea.icon + '" height="10"/><span>' + dsiArea.title + '</span></a>'));
+    var areaLink = $('<a style="color2:' + dsiArea.color + '"><img src="' + dsiArea.icon + '" height="10"/><span>' + dsiArea.title + '</span></a>');
+    areaLink.on('mouseover', function() {
+      tooltip.show();
+      tooltip.html('<h4>' + dsiArea.title +'</h4>' + dsiArea.info);
+    })
+    areaLink.on('mouseout', function() {
+      tooltip.hide();
+    })
+    sideBar.append(areaLink);
   })
 
   sideBar.append($('<h3>' + techTitle + '</h3>'));
 
   VizConfig.technologyFocuses.map(function(technologyFocus, technologyFocusIndex) {
-    sideBar.append($('<a><img src="' + technologyFocus.icon + '" height="12"/><span>' + technologyFocus.title + '</span></a>'));
+    var technologyLink = $('<a><img src="' + technologyFocus.icon + '" height="12"/><span>' + technologyFocus.title + '</span></a>');
+    technologyLink.on('mouseover', function() {
+      tooltip.show();
+      tooltip.html('<h4>' + technologyFocus.title +'</h4>' + technologyFocus.info);
+    })
+    technologyLink.on('mouseout', function() {
+      tooltip.hide();
+    })
+    sideBar.append(technologyLink);
   });
 
   var open = false;
@@ -37,7 +55,17 @@ function VizKey() {
       thumb.children('span').text('Hide Key');
     }
   });
+}
 
+VizKey.prototype.makeTooltip = function() {
+  var vizTooltip = $('<div id="vizTooltip"></div>');
+  $('body').append(vizTooltip);
+  vizTooltip.text('tooool tip');
 
-  console.log(vizKeyContainer)
+  $(window).on('mousemove', function(e) {
+    vizTooltip.css('left', e.pageX + 10);
+    vizTooltip.css('top', e.pageY);
+  });
+
+  return vizTooltip;
 }
