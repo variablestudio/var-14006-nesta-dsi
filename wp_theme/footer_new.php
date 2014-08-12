b206377357475472d641abb6df43524f08e79753
Marcin Ignac <marcin.ignac@gmail.com>
2014-08-12 17:21:54 +0100

    Org logos

--- wp_theme/footer_new.php
+++ wp_theme/footer_new.php
@@ -5,10 +5,14 @@
 		<div class='logos'>
 			<a href="http://www.nesta.org.uk/" title="Nesta"><img alt="Nesta" src="http://digitalsocial.eu/assets/partners/white/nesta-6a9b5fe999e8323b379ccc0d8e70290f.png" /></a>
 			<a href="http://www.esade.edu/" title="Esade Business School"><img alt="Esade" src="http://digitalsocial.eu/assets/partners/white/esade-b6eb89a49bfd702310d4e963f5f83695.png" /></a>
-			<a href="http://futureeverything.org/" title="FutureEverything"><img alt="Future-everything" src="http://digitalsocial.eu/assets/partners/white/future-everything-2f261cf2d078264179fd82b21e5927b7.png" /></a>
 			<a href="http://www.iri.centrepompidou.fr/en/" title="Institut de Recherche et d&#x27;Innovation"><img alt="Iri" src="http://digitalsocial.eu/assets/partners/white/iri-51848d69e8fcb9fa8c55089f574e311c.png" /></a>
 			<a href="http://waag.org/en" title="Waag Society"><img alt="Waag" src="http://digitalsocial.eu/assets/partners/white/waag-f1d052f43133268eaf2e13090a0b4bf1.png" /></a>
+		</div>
+		<div class='logos'>
+			<a href="http://futureeverything.org/" title="FutureEverything"><img alt="Future-everything" src="http://digitalsocial.eu/assets/partners/white/future-everything-2f261cf2d078264179fd82b21e5927b7.png" /></a>
 			<a href="http://www.swirrl.com/" title="Swirrl"><img alt="Swirrl" src="http://digitalsocial.eu/assets/partners/white/swirrl-63c559d4c17da1273922275c1284d09b.png" /></a>
+			<a href="http://www.interago.co.uk" title="Interago"><img alt="Interago" src="http://content.digitalsocial.eu/wp-content/uploads/2014/08/interago116.png" /></a>
+			<a href="http://variable.io" title="Variable"><img alt="Variable" src="http://content.digitalsocial.eu/wp-content/uploads/2014/08/variableWhite116.png" /></a>
 		</div>
 
 		<div class='contact'>
